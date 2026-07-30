7ac73de0ee0f71aaaca9e523f010f88c762cef74
Anil <anilmedikonda37@gmail.com>
2026-07-30 13:02:06 +0530

    new line added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
        System.out.println("welcome to java");
        System.out.println("I already know how to build the logic");
        System.out.println("now i am laernin how to express logic in java");
+       System.out.println("new line");
     }
 }
