a7cc28cc3a0209ff4dcfd270e4c1619bbd8f3088
Anil <anilmedikonda37@gmail.com>
2026-08-04 12:26:55 +0530

    new program

---
+++ track/M02-Introduction_tojava/T02/P02.java
@@ -0,0 +1,15 @@
+public class P02 {
+    public static void main(String[] args){
+        int num=25;
+        if (num > 0){
+            System.out.println("The number is Positive");
+        }
+
+        else if(num < 0){
+            System.out.println("thr numer is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
